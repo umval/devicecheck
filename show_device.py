a70c96cfed1098f28410714bd677e201c8e1a1cd
umval <yayaonpc@gmail.com>
2025-07-20 22:48:37 +0200

    Update show_device.py

--- show_device.py
+++ show_device.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     scans = []
 
     print(f"""
-                                                                                        {Fore.WHITE}Crédit : #sendyourwomen
+                                                                                        {Fore.WHITE}Crédit : #umval
 
 {Fore.RED}            ## ##   ###  ##   ## ##   ##   ##           ### ##   ### ###  ### ###    ####    ## ##   ### ###
             ##   ##   ##  ##  ##   ##  ##   ##            ##  ##   ##  ##   ##  ##     ##    ##   ##   ##  ##
